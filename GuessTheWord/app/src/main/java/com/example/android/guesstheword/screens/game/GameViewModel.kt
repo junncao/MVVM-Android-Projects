571fe4033cfc1e2c54023a9ce556a4917ba28af2
caojun8101 <991922377@qq.com>
2020-06-08 12:45:28 +0800

    MVVM practice

---
+++ GuessTheWord/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -0,0 +1,146 @@
+package com.example.android.guesstheword.screens.game
+
+import android.os.CountDownTimer
+import android.text.format.DateUtils
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+
+class GameViewModel:ViewModel() {
+    // The current word
+
+    private val _word = MutableLiveData<String>()
+    val word :LiveData<String>
+    get()=_word
+    // The current score
+
+    private val _score = MutableLiveData<Int>()
+    val score:LiveData<Int>
+        get()=_score
+
+    private val _eventGameFinish=MutableLiveData<Boolean>()
+    val eventGameFinish:LiveData<Boolean>
+    get() = _eventGameFinish
+    // The list of words - the front of the list is the next word to guess
+    private lateinit var wordList: MutableList<String>
+
+    private val _currentTime = MutableLiveData<Long>()
+    val currentTime: LiveData<Long>
+        get() = _currentTime
+
+    private val timer: CountDownTimer
+
+    val currentTimeString=Transformations.map(currentTime){
+        time->DateUtils.formatElapsedTime(time)
+    }
+    // The Hint for the current word
+    val wordHint = Transformations.map(word) { word ->
+        val randomPosition = (1..word.length).random()
+        "Current word has " + word.length + " letters" +
+                "\nThe letter at position " + randomPosition + " is " +
+                word.get(randomPosition - 1).toUpperCase()
+    }
+    companion object {
+
+        // Time when the game is over
+        private const val DONE = 0L
+
+        // Countdown time interval
+        private const val ONE_SECOND = 1000L
+
+        // Total time for the game
+        private const val COUNTDOWN_TIME = 60000L
+
+    }
+
+    init {
+        _word.value=""
+        _score.value=0
+        resetList()
+        nextWord()
+
+        timer = object : CountDownTimer(COUNTDOWN_TIME, ONE_SECOND) {
+            override fun onTick(millisUntilFinished: Long) {
+                _currentTime.value = millisUntilFinished/ONE_SECOND
+            }
+
+            override fun onFinish() {
+                _currentTime.value = DONE
+                onGameFinish()
+            }
+        }
+
+        timer.start()
+    }
+
+
+    /**
+     * Moves to the next word in the list
+     */
+    private fun nextWord() {
+        if (!wordList.isEmpty()) {
+            //Select and remove a word from the list
+            _word.value = wordList.removeAt(0)
+        }else{
+            resetList()
+        }
+
+    }
+
+
+    /**
+     * Resets the list of words and randomizes the order
+     */
+    private fun resetList() {
+        wordList = mutableListOf(
+                "queen",
+                "hospital",
+                "basketball",
+                "cat",
+                "change",
+                "snail",
+                "soup",
+                "calendar",
+                "sad",
+                "desk",
+                "guitar",
+                "home",
+                "railway",
+                "zebra",
+                "jelly",
+                "car",
+                "crow",
+                "trade",
+                "bag",
+                "roll",
+                "bubble"
+        )
+        wordList.shuffle()
+    }
+
+    /** Methods for buttons presses **/
+
+    fun onSkip() {
+        _score.value= score.value?.minus(1)
+        nextWord()
+    }
+
+    fun onCorrect() {
+        _score.value=score.value?.plus(1)
+        nextWord()
+    }
+
+
+    override fun onCleared() {
+        super.onCleared()
+        timer.cancel()
+    }
+    fun onGameFinish(){
+        _eventGameFinish.value=true
+    }
+    fun onGameFinishComplete(){
+        _eventGameFinish.value=false
+    }
+}
